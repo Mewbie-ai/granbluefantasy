b8394521d19c043deb22f6a579fe7bc4eacd24d6
Michael Jordan <mewbie3@gmail.com>
2021-12-24 22:35:49 +0700

    js update

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+// constants
 const modal = document.querySelector(".modal")
 const modalLink = document.querySelector(".modal-link")
-// const modalClose = document.querySelector(".modal-close")
+const modalClose = document.querySelector(".modal-close")
+const tab = document.querySelector(".tab")
+const tabLink = document.querySelector(".tab-link")
+const tabArrow = document.getElementById("tab-arrow")
 
+// variables
+let arrowTog = true
+
+modalClose.onclick = function() {
+  modal.style.display = "none";
+}
 modalLink.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "flex";
 }
 
-// modalClose.onclick = function() {
-//   modal.style.display = "none";
-// }
+tabLink.onclick = function() {
+  tab.classList.toggle("active");
+  tabArrow.classList.toggle("active");
+  arrowTog = !arrowTog;
+  if (arrowTog == true) {
+    tabArrow.src = 'links_tab.png'
+  } else {
+    tabArrow.src = 'links_tab_1.png'
+  }
+}
+
 
 window.onclick = function(event) {
   if (event.target == modal) {
